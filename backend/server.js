e95aecf7422ae1d00a1d7394d37381ba53274f02
YuLiRealMan <yulirealman@gmail.com>
2025-03-15 21:12:52 -0500

    post product route complete

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,35 @@
 import express from 'express'; // need to go to package.json and add "type": "module" to the json file
 
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 // console.log(process.env.MONGO_URI); // this will print the mongo_uri to the console
 
 const app = express();
-app.get('/products', (req, res) => {
-  res.send('Server is ready');
+app.use(express.json());    // this will allow us to parse the incoming json data
+
+app.post('/api/products', async (req, res) => {
+  console.log(req.body);
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({  success:false, message: 'All fields are required' });
+  }
+
+
+  const newProduct = new Product(product)
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, product: newProduct });
+  } catch (error) {
+    console.log("Error in saving product to the database: ", error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
 });
 
-
+app.post('/products', (req, res) => {
+  res.send('Server is ready');
+});
 
 app.listen(5000, () => {
     connectDB();
