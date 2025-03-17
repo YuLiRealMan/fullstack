771c16d80984f734b726f36fb84196b369e2e3b8
YuLiRealMan <yulirealman@gmail.com>
2025-03-16 21:41:54 -0500

    frontend navigation bar complete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import { Container, Flex, Text, HStack, Button, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaPlusSquare } from 'react-icons/fa';
+import { IoMoon } from 'react-icons/io5';
+import {LuSun} from 'react-icons/lu';
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>Navbar</div>
+    <Container maxW="1140px" px={4} >
+      <Flex
+        h={16}
+        alignItems="center"
+        justifyContent="space-between"
+        flexDir={{ base: 'column', sm: 'row' }}
+      >
+      <Text
+        bgGradient='linear(to-l, #7928CA, #FF0080)'
+        bgClip='text'
+        fontSize='6xl'
+        fontWeight='extrabold'
+      >
+
+
+        <Link to="/">Product Store 🛒</Link>
+      </Text>
+     
+
+        <HStack spacing={2} alignItems="center">
+          <Link to={"/create"}>
+            <Button>
+              <FaPlusSquare />
+            </Button>
+          </Link>
+
+          <Button onClick={toggleColorMode}>{colorMode==="light"? <IoMoon/>:<LuSun size="20"/>}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react'
 import { createRoot } from 'react-dom/client'
@@ -8,9 +8,9 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
       <BrowserRouter>
-        <Provider>
+        <ChakraProvider>
           <App />
-        </Provider>
+        </ChakraProvider>
       </BrowserRouter>
 
   </React.StrictMode>
